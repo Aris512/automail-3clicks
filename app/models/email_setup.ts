473ccb4526e3ffd2df409b25e7d656ec3f5c23fb
Javier <javier.toro2301@alumnos.ubiobio.cl>
2025-10-10 12:13:49 -0300

    avance en base de datos

--- app/models/email_setup.ts
+++ app/models/email_setup.ts
@@ -1,15 +1,52 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasOne } from '@adonisjs/lucid/orm'
+import Tenant from './tenant.js'
+import User from './user.js'
+import SmtpConfig from './smtp_config.js'
+
 
 export default class EmailSetup extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  
+  @column()
+  declare tenantId: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare email: string
+
+  @column()
+  declare name: string | null
+
+  @column()
+  declare from: string | null
+
+  @column()
+  declare active: boolean
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+
+  // relaciones
+  @belongsTo(() => Tenant, {
+    foreignKey: 'tenantId'
+  })
+  declare tenant: any
+
+  @belongsTo(() => User, {
+    foreignKey: 'userId'
+  })
+  declare user: any
+
+  @hasOne(() => SmtpConfig, {
+    foreignKey: 'emailSetupId'
+  })
+  declare smtpConfig: any
 }
--- app/models/smtp_config.ts
+++ app/models/smtp_config.ts
@@ -1,13 +1,38 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import EmailSetup from './email_setup.js'
 
 export default class SmtpConfig extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare emailSetupId: number
+
+  @column()
+  declare user: string
+
+  @column()
+  declare password: string
+
+  @column()
+  declare host: string
+
+  @column()
+  declare port: string
+
+  @column()
+  declare protocole: 'insecure' | 'ssl' | 'tls'
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  // relaciones
+  @belongsTo(() => EmailSetup, {
+    foreignKey: 'emailSetupId'
+  })
+  declare emailSetup: any
 }
