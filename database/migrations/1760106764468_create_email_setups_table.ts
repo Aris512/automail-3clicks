dc4f15e8151cc902a7908a65c13d5829b390a4f2
Javier <javier.toro2301@alumnos.ubiobio.cl>
2025-10-10 11:45:39 -0300

    tabla, base de datos email_setups y smtp_config incluidas

---
+++ database/migrations/1760106764468_create_email_setups_table.ts
@@ -0,0 +1,38 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'email_setups'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table
+      .integer('tenant_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('tenants')
+      .onDelete('CASCADE')
+
+      table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+
+      table.string('email', 255).notNullable()
+      table.string('name', 255).notNullable()
+      table.string('from', 255).notNullable()
+      table.boolean('active').notNullable().defaultTo(true)
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1760107136771_create_smtp_configs_table.ts
@@ -0,0 +1,30 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'smtp_configs'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      
+      table
+      .integer('email_setup_id')
+      .unsigned()
+      .notNullable()
+      .unique()
+      .references('id')
+      .inTable('email_setups')
+      .onDelete('CASCADE')
+
+      table.string('user', 255).notNullable()
+      table.string('password', 255).notNullable()
+      table.string('host', 255).notNullable()
+      table.string('port', 255).notNullable()
+      table.enum('protocole', ['insecure', 'ssl', 'tls']).notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
